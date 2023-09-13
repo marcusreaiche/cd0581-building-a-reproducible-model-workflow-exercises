ef782a4d30ba80a0397a6af84467182ad0820b7f
Marcus Reaiche <marcusreaiche@gmail.com>
2023-09-13 16:09:53 -0400

    Fix main.py to avoid mlflow too long parameters

--- lesson-4-training-validation-experiment-tracking/exercises/exercise_10/starter/main.py
+++ lesson-4-training-validation-experiment-tracking/exercises/exercise_10/starter/main.py
@@ -16,19 +16,21 @@ def go(config: DictConfig):
 
     # You can get the path at the root of the MLflow project with this:
     root_path = hydra.utils.get_original_cwd()
-
+    random_forest_path = os.path.join(root_path, "random_forest")
     # Serialize random forest configuration
     model_config = os.path.abspath("random_forest_config.json")
+    model_config_rel_path_from_model_path = \
+        os.path.relpath(model_config, start=random_forest_path)
 
     with open(model_config, "w+") as fp:
         json.dump(dict(config["random_forest"]), fp)
 
     _ = mlflow.run(
-        os.path.join(root_path, "random_forest"),
+        random_forest_path,
         "main",
         parameters={
             "train_data": config["data"]["train_data"],
-            "model_config": model_config
+            "model_config": model_config_rel_path_from_model_path
         },
     )
 
