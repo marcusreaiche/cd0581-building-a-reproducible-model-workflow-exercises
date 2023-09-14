d63c4133ced6b298d31be7ad7870b7574980e256
Marcus Reaiche <marcusreaiche@gmail.com>
2023-09-13 21:14:49 -0400

    Fix model_config path

--- lesson-4-training-validation-experiment-tracking/exercises/exercise_11/starter/main.py
+++ lesson-4-training-validation-experiment-tracking/exercises/exercise_11/starter/main.py
@@ -16,19 +16,22 @@ def go(config: DictConfig):
 
     # You can get the path at the root of the MLflow project with this:
     root_path = hydra.utils.get_original_cwd()
+    model_path = os.path.join(root_path, "random_forest")
 
     # Serialize decision tree configuration
     model_config = os.path.abspath("random_forest_config.yml")
+    model_config_rel_path_from_model_path = os.path.relpath(path=model_config,
+                                                            start=model_path)
 
     with open(model_config, "w+") as fp:
         fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
     _ = mlflow.run(
-        os.path.join(root_path, "random_forest"),
+        model_path,
         "main",
         parameters={
             "train_data": config["data"]["train_data"],
-            "model_config": model_config
+            "model_config": model_config_rel_path_from_model_path
         },
     )
 
